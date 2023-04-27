23a574b857b70eeef71969eb2610df5775bf33a6
Olzhas Alimseitov <olzhas547@gmail.com>
2023-04-27 09:24:11 +0600

    Add function templates

--- main.go
+++ main.go
@@ -12,3 +12,26 @@ type Course {
     ID int
     grade float64
 }
+
+func (s Student) addCourse {
+    return 0
+}
+
+func (s Student) viewCourse {
+    return 0
+}
+
+func (s Student) updateCourse {
+    return 0
+}
+func (s Student) deleteCourse {
+    return 0
+}
+
+func (c Course) getCourceCode {
+    return 0
+}
+
+func (c Course) getStudentGrade {
+    return 0
+}
